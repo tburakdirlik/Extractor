90ae2c9358ca5119ee3be80d274dbffce8de0d16
Burak Dirlik <87035514+tburakdirlik@users.noreply.github.com>
2023-05-10 14:19:20 +0300

    Update subnetExtractor.py

--- subnetExtractor.py
+++ subnetExtractor.py
@@ -1,8 +1,18 @@
 import ipaddress
+import sys
 
-subnet_list = input("Subnet Listesi Giriniz ").split()
-print("\n")
-for subnet_str in subnet_list:
+# dosya yolu parametresini alın
+dosya_yolu = sys.argv[1]
+
+# dosyayı açın ve içeriği metin değişkenine atayın
+with open(dosya_yolu, 'r') as dosya:
+    metin = dosya.read()
+
+# metindeki subnetleri ayıklayın
+subnet_listesi = metin.split()
+
+# subnetleri ekrana yazdırın
+for subnet_str in subnet_listesi:
     try:
         subnet = ipaddress.ip_network(subnet_str)
         for ip in subnet:
